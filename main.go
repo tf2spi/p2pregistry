f8d06d60deae81c7fd02f6b0a810a1791124d25c
misomosi <misomosispi@gmail.com>
2024-01-04 01:37:21 -0500

    Refactor broadcast function

--- main.go
+++ main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"errors"
-	"net"
 	"fmt"
 	"log"
+	"net"
 	"net/http"
 	"strconv"
 )
@@ -23,43 +23,62 @@ func getRemoteIp(addr string) (net.IP, error) {
 	return ip, errors.New(fmt.Sprintf("IP of remote addr '%s' not found!", addr))
 }
 
+func handleBroadcast(w http.ResponseWriter, r *http.Request) (int, error) {
+	err := r.ParseForm()
+	if err != nil {
+		return http.StatusBadRequest, err
+	}
+	ports, protocols := r.Form["port"], r.Form["protocol"]
+	if len(ports) != 1 || len(protocols) != 1 {
+		return http.StatusBadRequest,
+			errors.New(fmt.Sprintf("Expected exactly 1 protocol and port, got %d protocols and %d ports",
+				len(protocols), len(ports)))
+	}
+	port, err := strconv.Atoi(ports[0])
+	if err != nil {
+		return http.StatusBadRequest, err
+	}
+	protocol := protocols[0]
+	if protocol != "tls" && protocol != "tcp" && protocol != "udp" {
+		return http.StatusBadRequest,
+			errors.New(fmt.Sprintf("Expected 'tls', 'tcp', or 'udp' protocol, got '%s'", protocol))
+	}
+	ip, err := getRemoteIp(r.RemoteAddr)
+	if err != nil {
+		return http.StatusInternalServerError, err
+	}
+	log.Printf("Your address: %s\n", ip.String())
+	log.Printf("%d : %s\n", port, protocol)
+	log.Printf("Will save these in a database eventually...\n")
+	return http.StatusOK, nil
+}
+
+func handleQuery(w http.ResponseWriter, r *http.Request) (int, error) {
+	log.Print("We'll handle queries eventually here...")
+	// Filter ideas:
+	// * Filter by timestamp so only newer entries are shown
+	// * Filter to return only ipv6 or ipv4 addresses
+	// * Filter based on ip address, port, and transport layer protocol
+	return http.StatusOK, nil
+}
+
 func main() {
 	http.HandleFunc("/broadcast", func(w http.ResponseWriter, r *http.Request) {
-		err := r.ParseForm()
+		code, err := handleBroadcast(w, r)
+		w.WriteHeader(code)
 		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			log.Print(err)
+			log.Printf("[ERROR /broadcast]: %s", err)
 			fmt.Fprint(w, err)
-			return
 		}
-		ports, protocols := r.Form["port"], r.Form["protocol"]
-		if len(ports) != 1 || len(protocols) != 1 {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintf(w, "Expected exactly 1 protocol and port, got %d protocols and %d ports",
-				len(protocols), len(ports))
-			return
-		}
-		port, err := strconv.Atoi(ports[0])
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintf(w, "Unable to parse port as integer: %s", err)
-			return
-		}
-		protocol := protocols[0]
-		if protocol != "tcp" && protocol != "udp" {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintf(w, "Expected 'tcp' or 'udp' protocol, got '%s'", protocol)
-			return
-		}
-		ip, err := getRemoteIp(r.RemoteAddr)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			fmt.Fprint(w,err)
-			return
-		}
-		fmt.Fprintf(w, "Your address: %s\n", ip.String())
-		fmt.Fprintf(w, "%d : %s\n", port, protocol)
-		fmt.Fprintf(w, "Will save these in a database eventually...\n")
 	})
+	http.HandleFunc("/query", func(w http.ResponseWriter, r *http.Request) {
+		code, err := handleQuery(w, r)
+		w.WriteHeader(code)
+		if err != nil {
+			log.Printf("[ERROR /query]: %s", err)
+			fmt.Fprint(w, err)
+		}
+	})
+	log.Print("Listening on :8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
