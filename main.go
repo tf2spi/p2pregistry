f7167a580ef29a1f8e18ba95a9ce3e417160fe82
misomosi <misomosispi@gmail.com>
2024-01-19 20:32:47 -0500

    Add expiration logic

--- main.go
+++ main.go
@@ -9,58 +9,77 @@ import (
 	"net/http"
 	"strconv"
 	"time"
-	"reflect"
 	"strings"
 )
 
+type PeerTime int64
+
+// I'd like to use generics to optimize IPv4 addresses
+// to be [4]byte when specified but Go gives me a hard
+// time with that right now...
 type PeerDatabase struct {
+	Lookup map[[16]byte]PeerTime
 	Mutex sync.RWMutex
-	Ip []net.IP
-	Timestamp []int32
-	Expires []uint8
 };
 
-func (database *PeerDatabase) ContainsKey(ip net.IP) bool {
-	database.Mutex.RLock()
-	defer database.Mutex.RUnlock()
-	for _, dataip := range database.Ip {
-		if reflect.DeepEqual(dataip, ip) {
-			log.Print("IP already in database\n")
-			return true
-		}
-	}
-	return false
+func makePeerTime(timestamp int32, expires uint8) PeerTime {
+	return PeerTime(int64(timestamp) << 32 | int64(expires))
+}
+
+func (peertime PeerTime) Fields() (int32, uint8) {
+	return int32(peertime >> 32), uint8(peertime & 0xff)
 }
 
 func (database *PeerDatabase) Add(ip net.IP, timestamp int32, expires uint8) {
-	if (database.ContainsKey(ip)) {
-		return
-	}
 	database.Mutex.Lock()
 	defer database.Mutex.Unlock()
-	database.Ip = append(database.Ip, ip)
-	database.Timestamp = append(database.Timestamp, timestamp)
-	database.Expires = append(database.Expires, expires)
+	var ipbytes [16]byte
+	copy(ipbytes[:], ip)
+	peertime, ok := database.Lookup[ipbytes]
+	if ok {
+		prevstamp, _ := peertime.Fields()
+		database.Lookup[ipbytes] = makePeerTime(prevstamp, expires)
+	} else {
+		database.Lookup[ipbytes] = makePeerTime(timestamp, expires)
+	}
 }
 
-func (database *PeerDatabase) Dump(lower int32) string {
+func (database *PeerDatabase) Tick(delta uint8) {
+	database.Mutex.Lock()
+	defer database.Mutex.Unlock()
+
+	for ip, peertime := range database.Lookup {
+		timestamp, expires := peertime.Fields()
+		if expires <= delta {
+			log.Printf("%v expired!", ip);
+			delete(database.Lookup, ip)
+		} else {
+			database.Lookup[ip] = makePeerTime(timestamp, expires - delta)
+		}
+	}
+}
+
+func (database *PeerDatabase) Dump(lower int32, iplen int) string {
 	database.Mutex.RLock()
 	defer database.Mutex.RUnlock()
 	var response strings.Builder
-	for i := range database.Ip {
-		current := database.Timestamp[i]
+	for ip, peertime := range database.Lookup {
+		current, _ := peertime.Fields()
 		if current < lower {
 			continue
 		}
-		response.WriteString(fmt.Sprintf("\"%s\"", database.Ip[i]))
+		response.WriteString(fmt.Sprintf("\"%s\"", net.IP(ip[:iplen]).String()))
 		response.WriteString(",")
 	}
 	return response.String()
 }
 
-var v4database = PeerDatabase{};
-var v6database = PeerDatabase{};
-var srvepoch = time.Now();
+var v4Database = PeerDatabase{ Lookup: make(map[[16]byte]PeerTime) };
+var v6Database = PeerDatabase{ Lookup: make(map[[16]byte]PeerTime) };
+var srvEpoch = time.Now();
+
+var expireInit = uint8(60)
+var expirePeriod = uint8(10);
 
 func getRemoteIp(addr string) (net.IP, error) {
 	var ip []byte
@@ -82,18 +101,18 @@ func handleBroadcast(w http.ResponseWriter, r *http.Request) (int, error) {
 		return http.StatusBadRequest, err
 	}
 
-	timestamp := int32(time.Now().Sub(srvepoch) / 1_000_000_000)
+	timestamp := int32(time.Now().Sub(srvEpoch) / 1_000_000_000)
 
 	ip, err := getRemoteIp(r.RemoteAddr)
 	if err != nil {
 		return http.StatusInternalServerError, err
 	}
 
-	ip4, expires := ip.To4(), uint8(60)
+	ip4, expires := ip.To4(), expireInit
 	if ip4 != nil {
-		v4database.Add(ip4, timestamp, expires)
+		v4Database.Add(ip4, timestamp, expires)
 	} else {
-		v6database.Add(ip, timestamp, expires)
+		v6Database.Add(ip, timestamp, expires)
 	}
 
 	fmt.Fprintf(w, "{\"ip\":\"%s\",\"timestamp\":%d,\"expires\":%d}",
@@ -108,7 +127,7 @@ func handleQuery(w http.ResponseWriter, r *http.Request) (int, error) {
 	}
 
 	timestamps, preferences := r.Form["timestamp"], r.Form["prefer"]
-	now := (time.Now().Sub(srvepoch) / 1_000_000_000)
+	now := (time.Now().Sub(srvEpoch) / 1_000_000_000)
 	var timestamp = int32(0)
 	var prefer = "4";
 
@@ -133,9 +152,9 @@ func handleQuery(w http.ResponseWriter, r *http.Request) (int, error) {
 	var response = ""
 	var peers = ""
 	if prefer == "4" {
-		peers = v4database.Dump(timestamp) + v6database.Dump(timestamp)
+		peers = v4Database.Dump(timestamp, 4) + v6Database.Dump(timestamp, 16)
 	} else {
-		peers = v6database.Dump(timestamp) + v4database.Dump(timestamp)
+		peers = v6Database.Dump(timestamp, 16) + v4Database.Dump(timestamp, 4)
 	}
 	if len(peers) != 0 {
 		peers = peers[:len(peers) - 1]
@@ -148,6 +167,18 @@ func handleQuery(w http.ResponseWriter, r *http.Request) (int, error) {
 }
 
 func main() {
+	go func() {
+		period := time.Duration(expirePeriod) * time.Second
+		timer := time.NewTimer(period)
+		for {
+			<-timer.C
+			log.Print("Expire period elapsed!\n")
+			v4Database.Tick(expirePeriod)
+			v6Database.Tick(expirePeriod)
+			timer.Reset(period)
+		}
+
+	}();
 	http.HandleFunc("/broadcast", func(w http.ResponseWriter, r *http.Request) {
 		code, err := handleBroadcast(w, r)
 		if err != nil {
